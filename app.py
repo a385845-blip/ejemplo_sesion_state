5fdb81f93b5b0f0841084b92e63eb519381cd3f8
Deyra Herrera <a385845@uach.mx>
2025-10-29 08:03:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
 import streamlit as st
 st.title("Ejemplo para usar session_state")
-if "key" not in st.session_state:
-  st.session_state["key"] = "value"
-  
-count = 0
-
-increment = st.button("Increment")
-if increment:
-  count += 1
-st.write("Count = ", count)
+if "count" not in st.session_state:
+  st.session_state["count"] = 0
 
 st.write(st.session_state)
 
